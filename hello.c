5f9193ad9599e484ff44936995c0f15198380d00
MWEST35 <mwest35@huskers.unl.edu>
2022-08-24 22:03:10 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Michael K West
+ *         mwest35@huskers.unl.edu
+ * Date: 2022/08/24
+ *
+ * A simple "My Name" program
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Michael K West");
+
+}
